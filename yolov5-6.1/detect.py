067979088be45f35ea32f8b2302cb3e08ea3f581
pw s <78252964+polarispw@users.noreply.github.com>
2022-04-27 10:02:29 +0800

    4.27

--- yolov5-6.1/detect.py
+++ yolov5-6.1/detect.py
@@ -216,7 +216,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
 def parse_opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--weights', nargs='+', type=str, default=ROOT / 'runs/train/exp8/weights/best.pt', help='model path(s)')
-    parser.add_argument('--source', type=str, default=ROOT / 'E:/git repositories/Fundus-Imaging-Diagnosis/datasets/test/images', help='file/dir/URL/glob, 0 for webcam')
+    parser.add_argument('--source', type=str, default=ROOT / 'E:/git repositories/Fundus-Imaging-Diagnosis/fovea_datasets_for_yolo/test/images', help='file/dir/URL/glob, 0 for webcam')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='(optional) dataset.yaml path')
     parser.add_argument('--imgsz', '--img', '--img-size', nargs='+', type=int, default=[640], help='inference size h,w')
     parser.add_argument('--conf-thres', type=float, default=0.25, help='confidence threshold')
