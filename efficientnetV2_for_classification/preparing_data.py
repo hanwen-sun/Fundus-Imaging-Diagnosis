d45a26c49430efabe78bc6dfe792dc28b3266982
pw s <78252964+polarispw@users.noreply.github.com>
2022-04-28 11:16:00 +0800

    4.28

--- efficientnetV2_for_classification/preparing_data.py
+++ efficientnetV2_for_classification/preparing_data.py
@@ -3,7 +3,7 @@ import cv2
 
 data = pd.read_csv("E:/git repositories/Fundus-Imaging-Diagnosis/原始数据集/任务三数据/Mess1_annotation_train.csv")
 source = "E:/train/"
-destination = "E:/Mess_datasets_for_efficientnetV2/"
+destination = "E:/git repositories/Fundus-Imaging-Diagnosis/Mess_datasets_for_efficientnetV2/"
 
 name = data.iloc[:,1]
 grade = data.iloc[:,4]
@@ -15,12 +15,12 @@ for x,y in z:
     try:
         img = cv2.imread(path)
         if y=="0":
-            cv2.imwrite(destination+"0/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+            cv2.imwrite(destination+"0/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 3])
         if y=="1":
-            cv2.imwrite(destination+"1/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+            cv2.imwrite(destination+"1/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 3])
         if y=="2":
-            cv2.imwrite(destination+"2/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+            cv2.imwrite(destination+"2/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 3])
         if y=="3":
-            cv2.imwrite(destination+"3/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 0])
+            cv2.imwrite(destination+"3/"+x+".png", img, [int(cv2.IMWRITE_PNG_COMPRESSION), 3])
     except:
         print(x)
